b0e980a19a2c6bd2cb1c03740462f1c59100120c
georgemac510 <georgemac510@gmail.com>
2023-02-04 11:24:18 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,92 @@
+## Hyperledger Fabric ERC20 chaincode deployment
+
+Many thanks to Akshay Kurhekar and his article [https://medium.com/@akshay.kurhekar1014662/erc20-token-implementation-in-hyperledger-fabric-103cbfc21379] to simplify this process.
+
+Hi, all very happy to share this new approach to create ERC20 Token using Hyperledger Fabric for private blockchain.
+
+### Now, lets get started.
+
+we will first look into the arc of Hyplerledger Fabric (HLF).
+then will see development setup along with creating chaincode for our Dapp.
+
+
+
+### 1. up network
+
+    check fabric binary:
+    run from any folder
+
+`   $ docker images`
+
+    if you find fabric images
+
+    cd token-erc-20
+`   $ ./networkUp.sh`
+
+    to deploy chaincode we need to run this script
+     cd token-erc-20
+`    $ ./deployChaincode.sh erc20Token`
+
+    -------------------------------
+
+    Now, lets move towards application part
+
+`    $ cd api_server` 
+`    $ npm install`
+
+`    $ node registerAdmin.js`
+
+    output:
+    Wallet path: /home/akshay/fabric/HLF-erc20Token-starter/token-erc-20/api-server/wallet
+    Successfully enrolled admin user "admin" and imported it into the wallet
+
+Register Minter and Reciever:
+
+`    $ node registerMinter.js`
+`    $ node registerRecevier.js`
+
+    we invoke chaincode by this user John we have setToken details and fech data from ledger.
+
+`    $ node invokeByMinter.js`
+    out put:
+    Token details ::  Alpha
+    Mint status ::
+    User balance ::
+    Transfer status ::
+    User balance after transfer :: 
+
+Run Receiver script to check Dina's balance:
+
+`    $ node invokeByRecevier.js`
+
+
+
+    
+    day - 2 
+
+    create chain code fun for mint, transfer along with balance and accountId
+
+    Note:
+    once you deploy chaincode, if you updated logic of chaincode and you want to use that logic
+    need to upgrade chaincode with new_cc_name.
+
+`   $ cd token-erc-20`
+
+`    $ ./upgradeChaincode.sh CC_Name`
+
+    After upgrading chaincode we will test our ERC20 token functions
+    
+    In node app by invokeByMinter script for minting 9000 Tokens Transfer it to different user and check balance .
+`   $node invokeByMinter.js`
+    To stop container
+`   $ docker stop` 
+
+    To down network or we can say that to close development setup we need to do.
+    
+`    $ cd token-erc-20`
+
+    check files you will get networkDown.sh script we need to execute it.
+
+`   $ ./networkDown.sh` 
+
+### Helpful articles
---
+++ token-erc-20/chaincode-javascript/lib/tokenERC20.js
@@ -0,0 +1,170 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+'use strict';
+
+const { Contract } = require('fabric-contract-api');
+const ClientIdentity = require('fabric-shim').ClientIdentity;
+
+
+const balancePrefix = 'balance';
+
+// table X
+// key - value
+// ERC20 token name : Alpha
+// Define key names for options
+const nameKey = 'name';
+const symbolKey = 'symbol';
+const decimalsKey = 'decimals';
+const totalSupplyKey = 'totalSupply';
+// let cid;
+
+class TokenERC20Contract extends Contract {
+    
+    async SetOption(ctx, name, symbol, decimals){
+        //buffer example: [ 23 45 57 79 89 90]
+        await ctx.stub.putState(nameKey, Buffer.from(name)) // to create a state on ledger
+        await ctx.stub.putState(symbolKey, Buffer.from(symbol)) // to create a state on ledger
+        await ctx.stub.putState(decimalsKey, Buffer.from(decimals)) // to create a state on ledger
+        return 'success';
+    }
+
+    async TokenName(ctx) {
+        const nameBytes = await ctx.stub.getState(nameKey);
+        return nameBytes.toString();
+    }
+
+    async Symbol(ctx) {
+        const symbolBytes = await ctx.stub.getState(symbolKey);
+        return symbolBytes.toString();
+    }
+
+    async Decimals(ctx) {
+        const decimalBytes = await ctx.stub.getState(decimalsKey);
+        return decimalBytes.toString();
+    }
+
+    async Mint(ctx, amount) {
+        
+        // validating user role
+        let cid = new ClientIdentity(ctx.stub)
+        const role = await cid.getAttributeValue('role'); // get role from cert of registered user.
+        
+        if(role !== 'Minter') {
+            return('User is not Authorized to Mint Tokens....!')   
+        }
+        
+        // validating amount
+        const amountInt = parseInt(amount);
+        if(amountInt < 0){
+            return('Amount should not be Zero....!');
+        }
+        
+        // increment balance of minter
+        const minter = await cid.getAttributeValue('userId'); // get userId from cert of registered user.
+        const balanceKey = ctx.stub.createCompositeKey(balancePrefix,[minter]);
+        
+        const currentBalanceBytes = await ctx.stub.getState(balanceKey);
+
+        let currentBalance;
+        if(!currentBalanceBytes || currentBalanceBytes.length === 0 ){
+            currentBalance = 0;
+        }else {
+            currentBalance = parseInt(currentBalanceBytes.toString());
+        }
+        const updatedBalance = currentBalance + amountInt;
+        await ctx.stub.putState(balanceKey, Buffer.from(updatedBalance.toString()));
+
+
+        // increment total supply
+        let totalSupply;
+        const totalSupplyBytes = await ctx.stub.getState(totalSupplyKey); // fetch the tokensupply value from ledger
+
+        if(!totalSupplyBytes || totalSupplyBytes.length === 0 ){
+            totalSupply = 0;
+            console.log('Initialize the tokenSupply..!');
+        }else {
+            totalSupply = parseInt(totalSupplyBytes.toString());
+        }
+        totalSupply = totalSupply + amountInt; // added tokenSupply with new amount
+
+        await ctx.stub.putState(totalSupplyKey, Buffer.from(totalSupply.toString())); // key- value
+
+        return 'success';
+    }
+
+    async getBalance(ctx) {
+
+        let balance;
+        let cid = new ClientIdentity(ctx.stub);
+        const userID = await cid.getAttributeValue('userId'); // get userId from cert of registered user.
+        const balanceKey = ctx.stub.createCompositeKey(balancePrefix,[userID]);
+        const currentBalanceBytes = await ctx.stub.getState(balanceKey);
+
+        if(!currentBalanceBytes || currentBalanceBytes.length === 0 || currentBalanceBytes === 0  ){
+            return(`This user ${userID} has Zero balance Account`);
+
+        }
+            balance = parseInt(currentBalanceBytes.toString());
+        
+
+        return balance;
+    }
+
+    async Transfer(ctx,to, value){
+       
+        let cid = new ClientIdentity(ctx.stub);
+        const from = await cid.getAttributeValue('userId'); // get sender userId from cert of registered user.
+        
+         // Convert value from string to int
+         const valueInt = parseInt(value);
+
+         if (valueInt < 0) { // transfer of 0 is allowed in ERC20, so just validate against negative amounts
+             return('transfer amount cannot be negative');
+         }
+ 
+         // Retrieve the current balance of the sender
+         const fromBalanceKey = ctx.stub.createCompositeKey(balancePrefix, [from]);
+         const fromCurrentBalanceBytes = await ctx.stub.getState(fromBalanceKey);
+ 
+         if (!fromCurrentBalanceBytes || fromCurrentBalanceBytes.length === 0) {
+             return(`client account ${from} has no balance`);
+         }
+ 
+         const fromCurrentBalance = parseInt(fromCurrentBalanceBytes.toString());
+ 
+         // Check if the sender has enough tokens to spend.
+         if (fromCurrentBalance < valueInt) {
+             return(`client account ${from} has insufficient funds.`);
+         }
+ 
+         // Retrieve the current balance of the recepient
+         const toBalanceKey = ctx.stub.createCompositeKey(balancePrefix, [to]);
+         const toCurrentBalanceBytes = await ctx.stub.getState(toBalanceKey);
+ 
+         let toCurrentBalance;
+         // If recipient current balance doesn't yet exist, we'll create it with a current balance of 0
+         if (!toCurrentBalanceBytes || toCurrentBalanceBytes.length === 0) {
+             toCurrentBalance = 0;
+         } else {
+             toCurrentBalance = parseInt(toCurrentBalanceBytes.toString());
+         }
+ 
+         // Update the balance
+         const fromUpdatedBalance = fromCurrentBalance - valueInt;
+         const toUpdatedBalance = toCurrentBalance + valueInt;
+ 
+         await ctx.stub.putState(fromBalanceKey, Buffer.from(fromUpdatedBalance.toString()));
+         await ctx.stub.putState(toBalanceKey, Buffer.from(toUpdatedBalance.toString()));
+ 
+         console.log(`client ${from} balance updated from ${fromCurrentBalance} to ${fromUpdatedBalance}`);
+         console.log(`recipient ${to} balance updated from ${toCurrentBalance} to ${toUpdatedBalance}`);
+ 
+         return true;
+
+    }
+
+}
+
+module.exports = TokenERC20Contract;
